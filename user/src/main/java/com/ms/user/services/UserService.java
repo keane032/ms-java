15510bb401bd0bfdd2be688233aa005b3db84403
royke <keane032@gmail.com>
2025-04-15 21:47:54 -0300

    primeiro commit

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,21 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    public UserModel save(UserModel user){
+        return userRepository.save(user);
+    }
+
+
+}
