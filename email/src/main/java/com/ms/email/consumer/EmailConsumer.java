59cfed997f01d22ce77e00f6561696e021d51ac5
royke <keane032@gmail.com>
2025-04-22 20:12:21 -0300

    email sent config

--- email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,16 +1,25 @@
 package com.ms.email.consumer;
 
 import com.ms.email.dtos.EmailRecorderDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    @Autowired
+    private EmailService emailService;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecorderDto  email){
-        System.out.println(email.emailTo());
+       EmailModel emailModel = new EmailModel();
+       BeanUtils.copyProperties(email, emailModel);
+       emailService.sendEmail(emailModel);
     }
 
 }
